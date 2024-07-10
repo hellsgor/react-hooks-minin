4770179b49211d8aeb915a76e56dafde8e56a40b
Igor Storchevoy <hellsgor@gmail.com>
2024-07-11 01:22:15 +0400

    add(Counter.jsx): created Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Counter from './Components/Counter/Counter';
 
 export default function App() {
   return (
     <>
       <h1>Hello, React Hooks!</h1>
+      <Counter />
     </>
   );
 }
