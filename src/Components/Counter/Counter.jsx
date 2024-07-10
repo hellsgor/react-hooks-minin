dc38fa3d87be21c04c29fc07de084eaf816aa08b
Igor Storchevoy <hellsgor@gmail.com>
2024-07-11 02:15:11 +0400

    update(Counter): added initial state calculation

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -1,11 +1,27 @@
 import { useState } from 'react';
 import './Counter.css';
 
+function computedInitialCounter() {
+  console.log('Some calculations...');
+  return 5 - 5;
+}
+
 export default function Counter() {
-  const [counter, setCounter] = useState(0);
+  // const [counter, setCounter] = useState(0);
+  // передано начальное значение счётчика в виде числа
+
+  // const [counter, setCounter] = useState(computedInitialCounter());
+  // предположим, что хотим вычислять начальное значение счётчика.
+  // функция computedInitialCounter будет запускаться каждый новый рендеринг, что увеличит используемые ресурсы
+
+  const [counter, setCounter] = useState(() => computedInitialCounter());
+  // передать в качестве начального значения call-back-функцию
+  // то начальное значение будет вычислено только один раз при первом рендере страницы
 
   function increment() {
     setCounter((prevCounter) => prevCounter + 1);
+    // setCounter возвращает предыдущее состояние
+    // и чтобы всегда работать именно с предыдущим состоянием можно использовать конструкцию выше
   }
 
   function decrement() {
