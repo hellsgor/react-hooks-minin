6b3b4701b66c222f007481013009eaa97e4662d1
Igor Storchevoy <hellsgor@gmail.com>
2024-07-11 01:53:50 +0400

    update(Counter): implemented counter logic

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -1,3 +1,30 @@
+import { useState } from 'react';
+import './Counter.css';
+
 export default function Counter() {
-  return <p>i am counter</p>;
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter((prevCounter) => prevCounter + 1);
+  }
+
+  function decrement() {
+    setCounter((prevCounter) => prevCounter - 1);
+  }
+
+  function doubleIncrement() {
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+  }
+
+  return (
+    <div className="counter">
+      <h3 className="counter-title">i am counter: {counter}</h3>
+      <div className="counter-wrapper">
+        <button onClick={increment}>inc</button>
+        <button onClick={doubleIncrement}>double inc</button>
+        <button onClick={decrement}>dec</button>
+      </div>
+    </div>
+  );
 }
