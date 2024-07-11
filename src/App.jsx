bb055068fc8b2812f3e18afd58081dc8b017f747
Igor Storchevoy <hellsgor@gmail.com>
2024-07-11 13:01:02 +0400

    add: ObjectState component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css';
 import Counter from './Components/Counter/Counter';
+import ObjectState from './Components/ObjectState/ObjectState';
 
 export default function App() {
   return (
     <>
       <h1>Hello, React Hooks!</h1>
-      <Counter />
+      <div className="row">
+        <h2 className="row__title">useState:</h2>
+        <Counter />
+        <ObjectState />
+      </div>
     </>
   );
 }
