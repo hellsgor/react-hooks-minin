3eff1635117d08acf9c06c44058582766f1d80fd
Igor Storchevoy <hellsgor@gmail.com>
2024-07-11 22:20:50 +0400

    add(Resource) create component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import './App.css';
 import Counter from './Components/Counter/Counter';
 import ObjectState from './Components/ObjectState/ObjectState';
+import Resource from './Components/Resource/Resource';
 
 export default function App() {
   return (
     <>
       <h1>Hello, React Hooks!</h1>
+
       <div className="row">
         <h2 className="row__title">useState:</h2>
-        <Counter />
-        <ObjectState />
+        <div className="wrapper">
+          <Counter />
+          <ObjectState />
+        </div>
+      </div>
+
+      <div className="row">
+        <h2 className="row__title">useEffect:</h2>
+        <div className="wrapper">
+          <Resource />
+        </div>
       </div>
     </>
   );
