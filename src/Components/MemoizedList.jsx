75ca8a6ec402d8cdf4453480df43fea12ebac5d5
Igor Storchevoy <hellsgor@gmail.com>
2025-02-13 00:12:55 +0400

    Feat[MemoizedList]: created component

---
+++ src/Components/MemoizedList.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { ItemsList } from './ItemsList';
+
+export default function MemoizedList() {
+  const [count, setCount] = useState(1);
+  const [colored, setColored] = useState(false);
+
+  const styles = {
+    width: '2rem',
+    height: '2rem',
+    borderRadius: '50%',
+    alignSelf: 'center',
+    backgroundColor: colored ? 'yellowgreen' : 'transparent',
+    border: '1px solid yellowgreen',
+    transition: 'background-color 0.3s ease-in-out',
+  };
+
+  const generateItemsFromAPI = () =>
+    new Array(count).fill('').map((_, i) => `Element ${i + 1}`);
+
+  return (
+    <div className="card">
+      <h3 className="card__title">i am NewComp</h3>
+      <h4>Number of elements: {count}</h4>
+
+      <div className="card__wrapper">
+        <button className="button" onClick={() => setCount((prev) => prev + 1)}>
+          inc
+        </button>
+
+        <ItemsList getItems={generateItemsFromAPI} />
+      </div>
+
+      <div className="card__wrapper">
+        <button className="button" onClick={() => setColored((prev) => !prev)}>
+          change color
+        </button>
+        <div style={styles}></div>
+      </div>
+    </div>
+  );
+}
