655bf7fbef5becee56f62259dc5d27f546c5b563
Igor Storchevoy <hellsgor@gmail.com>
2025-02-12 23:09:43 +0400

    Feat[MemoizedCounter]: create component (using useMemo)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import AutoFocus from './Components/AutoFocus';
 import Cords from './Components/Cords';
 import Counter from './Components/Counter';
+import MemoizedCounter from './Components/MemoizedCounter';
 import ObjectState from './Components/ObjectState';
 import PreviousValue from './Components/PreviousValue';
 import RenderCounter from './Components/RenderCounter';
@@ -34,6 +35,13 @@ export default function App() {
           <RenderCounter />
           <AutoFocus />
           <PreviousValue />
+        </div>
+      </div>
+
+      <div className="row">
+        <h2 className="row__title">useMemo:</h2>
+        <div className="wrapper">
+          <MemoizedCounter />
         </div>
       </div>
     </>
