136a5e940e1a4fbd3e15124a3451cc23d3833b21
Igor Storchevoy <hellsgor@gmail.com>
2025-02-12 14:40:37 +0400

    FIx[Resource]: fixed logs

--- src/Components/Resource.jsx
+++ src/Components/Resource.jsx
@@ -7,8 +7,8 @@ export default function Resource() {
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/${type}`)
       .then((response) => response.json())
-      .then((json) => setData(json))
-      .then(() => console.log(data));
+      .then((json) => setData(json));
+    // .then(() => console.log(data));
   }, [type]);
   // useEffect служит для каких-либо сайд-эффектов
   // то есть наблюдая за чем-то можно реагировать на изменения и выполнять какую-то логику
@@ -18,13 +18,22 @@ export default function Resource() {
       <h3 className="card__title">i am Resource</h3>
 
       <div className="resource__wrapper card__wrapper">
-        <button className={type === 'users' ? 'button button_active' : 'button'} onClick={() => setType('users')}>
+        <button
+          className={type === 'users' ? 'button button_active' : 'button'}
+          onClick={() => setType('users')}
+        >
           users
         </button>
-        <button className={type === 'todos' ? 'button button_active' : 'button'} onClick={() => setType('todos')}>
+        <button
+          className={type === 'todos' ? 'button button_active' : 'button'}
+          onClick={() => setType('todos')}
+        >
           todos
         </button>
-        <button className={type === 'posts' ? 'button button_active' : 'button'} onClick={() => setType('posts')}>
+        <button
+          className={type === 'posts' ? 'button button_active' : 'button'}
+          onClick={() => setType('posts')}
+        >
           posts
         </button>
       </div>
